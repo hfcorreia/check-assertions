14876be85678b85d77f103512c86fb331ade718b
Hugo Correia <hfcorreia91@gmail.com>
2013-03-23 12:36:37 +0000

    added new assertion for arrays

---
+++ src/ist/meic/pa/ArrayInitializationAssertion.java
@@ -0,0 +1,13 @@
+package ist.meic.pa;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE })
+
+public  @interface ArrayInitializationAssertion {
+    boolean value() default true;
+}
