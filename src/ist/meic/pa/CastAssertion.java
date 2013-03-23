89202913f7d408b7dacdd9050008c9e479cb7c61
Hugo Correia <hfcorreia91@gmail.com>
2013-03-23 12:34:28 +0000

    minor cleanup

--- src/ist/meic/pa/CastAssertion.java
+++ src/ist/meic/pa/CastAssertion.java
@@ -1,9 +1,12 @@
 package ist.meic.pa;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE})
+@Target({ ElementType.TYPE })
 public @interface CastAssertion {
-	String[] value();
-} 
+    String[] value();
+}
