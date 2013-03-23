cfa7876985032ad66890edc3572a7aaf8f43a9e3
Hugo Correia <hfcorreia91@gmail.com>
2013-03-23 12:35:00 +0000

    doing int arrays checks

--- src/ist/meic/pa/ArrayInterceptor.java
+++ src/ist/meic/pa/ArrayInterceptor.java
@@ -1,21 +1,42 @@
 package ist.meic.pa;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ArrayInterceptor {
-   
-    public ArrayInterceptor() {
-    }
+    public static HashMap<Object, ArrayList<Boolean>> arrays = new HashMap<Object, ArrayList<Boolean>>();
 
     public static void arrayWriteInt(Object obj, int index, int newValue) {
-       int[] array = (int[]) obj;
-       array[index] = newValue;
+        int[] array = (int[]) obj;
+        array[index] = newValue;
+        
+        ArrayList<Boolean> isInitialized = new ArrayList<Boolean>();
+        
+        for(int i = 0; i < array.length ; i++) {
+            if(i == index)
+                isInitialized.add(true);
+            else
+                isInitialized.add(false);
+        }
+        
+        arrays.put(obj, isInitialized);
     }
 
     public static int arrayReadInt(Object obj, int index) {
+       
+        if( !arrays.containsKey(obj) || !arrays.get(obj).get(index) )  {
+            throw new RuntimeException("Array is not initialized at " + index);
+        }            
         int[] array = (int[]) obj;
         return array[index];
     }
-    
+
+    public static Object arrayReadObject(Object obj, int index) {
+        if(obj.getClass().isArray())
+            System.out.println("sou Array");
+        return obj;
+    }
     public static void arrayWriteObject(Object obj, int index, Object newValue) {
+     
     }
 }
